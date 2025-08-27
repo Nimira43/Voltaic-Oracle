135544d054d60b71f5e64e62bb61a946c827f5f2
Nimira43 <n.rabone72@aol.com>
2025-08-27 21:40:06 +0100

    md5 hash

--- 01-project-intro/renderer.js
+++ 01-project-intro/renderer.js
@@ -3,5 +3,7 @@ $(() => {
 
   $(('#text-input').bind('input propertychange', () => {
     const text = this.value
+
+    const md5 = crypto.createHash('md5').update(text, 'utf8').digest('hex')
   }))
 })
