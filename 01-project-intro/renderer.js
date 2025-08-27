e76ebcbd81b45610582b2a8fea1c1e133b1e2a1b
Nimira43 <n.rabone72@aol.com>
2025-08-27 21:53:58 +0100

    Added JQuery link to Content Security Policy

--- 01-project-intro/renderer.js
+++ 01-project-intro/renderer.js
@@ -1,9 +1,10 @@
 $(() => {
   const crypto = require('crypto')
 
-  $(('#text-input').bind('input propertychange', () => {
+  $('#text-input').bind('input propertychange', () => {
     const text = this.value
 
     const md5 = crypto.createHash('md5').update(text, 'utf8').digest('hex')
-  }))
+    $('#md5-output').text(md5)
+  })
 })
