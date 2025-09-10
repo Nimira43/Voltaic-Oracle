b7d673f884b96f9a4378543e48416c1a1be93472
Nimira43 <n.rabone72@aol.com>
2025-09-10 03:38:13 +0100

    sha1 and sha256

--- 01-project-intro/renderer.js
+++ 01-project-intro/renderer.js
@@ -4,7 +4,22 @@ $(() => {
   $('#text-input').bind('input propertychange', function() {
     const text = this.value
 
-    const md5 = crypto.createHash('md5').update(text, 'utf8').digest('hex')
+    const md5 = crypto
+      .createHash('md5')
+      .update(text, 'utf8')
+      .digest('hex')
     $('#md5-output').text(md5)
+    
+    const sha1 = crypto
+      .createHash('sha1')
+      .update(text, 'utf8')
+      .digest('hex')
+    $('#sha1-output').text(md5)
+    
+    const sha256 = crypto
+      .createHash('sha256')
+      .update(text, 'utf8')
+      .digest('hex')
+    $('#sha256-output').text(md5)
   })
 })
