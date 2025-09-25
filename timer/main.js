91fc0374fd21262a6c21d4bbff9d016e7de0da05
Nimira43 <n.rabone72@aol.com>
2025-09-25 11:25:58 +0100

    Added Menu, shell and ipcMain requirements from Electron

--- timer/main.js
+++ timer/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu, shell, ipcMain } = require('electron')
 const path = require('node:path')
 
 function createWindow () {
