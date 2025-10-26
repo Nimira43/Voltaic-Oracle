02d262402969e2f56493642637ba270eba9dc115
Nimira43 <n.rabone72@aol.com>
2025-10-26 01:04:45 +0100

    renderer - 1

---
+++ code-editor/renderer.js
@@ -0,0 +1,11 @@
+const { ipcRenderer } = require('electron')
+
+let newButton, saveButton, openButton
+let editor
+
+const updateInfo = () => {}
+const handleNewButton = () => {}
+const handleSaveButton = () => {}
+const handleOpenButton = () => {}
+
+onload = function() {}
